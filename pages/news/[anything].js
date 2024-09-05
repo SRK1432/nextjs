2c07f06d681a6a6bc5012e73df7f05648ec3d98a
SRK1432 <kshrinivas309@gmail.com>
2024-09-05 14:09:14 +0530

    3

--- pages/news/[anything].js
+++ pages/news/[anything].js
@@ -1,4 +1,8 @@
+import { useRouter } from "next/router";
+
 function DetailPage(){
+    const router = useRouter();
+    const newsId = router.query.newsId;
     return<h2>The Details Page</h2>
 }
 export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,14 @@
+import Link from 'next/link';
 function NewsPage(){
-    return<h2>The News Page</h2>
+    return(
+        <>
+         <h2>The News Page</h2>
+         <ul>
+            <li><Link href='/news/nextjs-is-a-great-framework'>Nextjs is a great framework</Link></li>
+            <li>something else</li>
+         </ul>
+        </>
+
+)
 }
 export default NewsPage;
